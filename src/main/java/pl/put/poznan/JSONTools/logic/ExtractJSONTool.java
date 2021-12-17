39b987e963440edd869a9a7822802fc4bbb740b0
Grzegorz Piatek <grzegorzadampiatek@gmail.com>
2021-12-17 23:41:51 +0100

    Extract ExtractJSONTool

--- src/main/java/pl/put/poznan/JSONTools/logic/ExtractJSONTool.java
+++ src/main/java/pl/put/poznan/JSONTools/logic/ExtractJSONTool.java
@@ -1,2 +1,17 @@
-package pl.put.poznan.JSONTools.logic;public class ExtractJSONTool {
+package pl.put.poznan.JSONTools.logic;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+
+
+public class ExtractJSONTool extends JSONTool {
+
+    private String jsonMinified;
+    private String jsonPretty;
+
+    public ExtractJSONTool(String file) {
+        super(file);
+    }
+
 }
