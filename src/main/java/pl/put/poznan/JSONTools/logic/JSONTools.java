fea71d94d686afac9d8102b5754f14bf8394a500
Grzegorz Piatek <grzegorzadampiatek@gmail.com>
2021-12-17 11:53:44 +0100

    Add constructor of JSON object

--- src/main/java/pl/put/poznan/JSONTools/logic/JSONTools.java
+++ src/main/java/pl/put/poznan/JSONTools/logic/JSONTools.java
@@ -1,22 +1,22 @@
 package pl.put.poznan.JSONTools.logic;
 
 import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 
 /**
  * This is just an example to show that the logic should be outside the REST service.
  */
 public class JSONTools {
 
-    private ObjectMapper mapper = new ObjectMapper();
+    private ObjectMapper mapper;
+    private JsonNode jsonObject;
 
-    public JSONTools(String[] transforms){
-        this.mapper = transforms;
-    }
-
-    public String transform(String text){
-        // of course, normally it would do something based on the transforms
-        return text.toUpperCase();
+    public JSONTools(String file) throws JsonProcessingException {
+        mapper = new ObjectMapper();
+        jsonObject = mapper.readTree(file);
     }
 }
