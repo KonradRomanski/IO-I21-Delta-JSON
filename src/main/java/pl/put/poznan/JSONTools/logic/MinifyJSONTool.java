ec1db014fdb2b0a502ef6333f5e58c997ddb046d
Grzegorz Piatek <grzegorzadampiatek@gmail.com>
2021-12-17 23:41:30 +0100

    Extract MinifyJSONTool

--- src/main/java/pl/put/poznan/JSONTools/logic/MinifyJSONTool.java
+++ src/main/java/pl/put/poznan/JSONTools/logic/MinifyJSONTool.java
@@ -1,2 +1,50 @@
-package pl.put.poznan.JSONTools.logic;public class MinifyJSONTool {
+package pl.put.poznan.JSONTools.logic;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+
+
+public class MinifyJSONTool extends JSONTool {
+
+    private String jsonMinified;
+    private String jsonPretty;
+
+    public MinifyJSONTool(String file) {
+        super(file);
+    }
+
+    private void minify() throws JsonProcessingException {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.enable(SerializationFeature.CLOSE_CLOSEABLE);
+        jsonMinified = mapper.writeValueAsString(super.getJsonObject());
+    }
+
+    private void prettify() throws JsonProcessingException {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.enable(SerializationFeature.INDENT_OUTPUT);
+        jsonPretty = mapper.writeValueAsString(super.getJsonObject());
+    }
+
+    public String getMinified() {
+        if (jsonMinified == null){
+            try {
+                this.minify();
+            } catch (JsonProcessingException e) {
+                e.printStackTrace();
+            }
+        }
+        return jsonMinified;
+    }
+
+    public String getPretty() {
+        if (jsonPretty == null){
+            try {
+                this.prettify();
+            } catch (JsonProcessingException e) {
+                e.printStackTrace();
+            }
+        }
+        return jsonPretty;
+    }
 }
