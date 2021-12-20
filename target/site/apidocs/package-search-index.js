d6a0e895dea4699c46785c32faccc5040a7c4f47
Evarios <jan.brozdziak@gmail.com>
2021-12-20 23:52:44 +0100

    test auto generating javadoc

---
+++ target/site/apidocs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"pl.put.poznan.JSONTools.app"},{"l":"pl.put.poznan.JSONTools.logic"},{"l":"pl.put.poznan.JSONTools.rest"}];updateSearchResults();
