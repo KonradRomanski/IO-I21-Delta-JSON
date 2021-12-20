1ff4a67d195134ad1818f14bfdd15d8f6fe330f4
Evarios <jan.brozdziak@gmail.com>
2021-12-21 00:26:23 +0100

    cleanup

--- target/site/apidocs/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"pl.put.poznan.JSONTools.app"},{"l":"pl.put.poznan.JSONTools.logic"},{"l":"pl.put.poznan.JSONTools.rest"}];updateSearchResults();
